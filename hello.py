d6d8801a7eb34ce58543d2ae23ea13025e0e0f74
satyasai3333 <satyasaibablu087@gmail.com>
2025-07-01 03:12:03 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:")
+print("Welcome," + name)
+Birth_year = input("Enter your birth year")
+current_year = input("Enter your current year")
+current_age = current_year-Birth_year
+print("age:", current_age)
